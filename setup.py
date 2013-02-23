7780cf8306d92aa9128924dd52d5e56c81e19244
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2013-02-24 00:33:48 +0600

    bump version number - include readme and license fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='doc-versions',
-      version='0.1',
+      version='0.1.1',
       description='Django app for tracking changes in models',
       author='CHTD',
       license='MIT',
